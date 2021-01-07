f49f31ca74097623fff8e1611cd2c0a08f0150a1
Kai Xu <k.xu@mdx.ac.uk>
2021-01-07 23:47:25 +0000

    first commit

---
+++ products.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "images/yarn.jpg",
+      "availableInventory": 7,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Kitty Litter",
+      "description": "Premium kitty litter for your cat.",
+      "price": 11.00,
+      "image": "images/cat-litter.jpg",
+      "availableInventory": 99,
+      "rating": 4
+    },
+    {
+      "id": 1004,
+      "title": "Cat House",
+      "description": "A place for your cat to play!",
+      "price": 7.99,
+      "image": "images/cat-house.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 1005,
+      "title": "Laser Pointer",
+      "description": "Drive your cat crazy with this <em>amazing</em> product.",
+      "price": 49.99,
+      "image": "images/laser-pointer.jpg",
+      "availableInventory": 25,
+      "rating": 4
+    }
+  ]

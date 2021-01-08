67dafef009c2b0dfbee3954fb1085891cdec9218
Kai Xu <k.xu@mdx.ac.uk>
2021-01-08 16:57:00 +0000

    added retrieve from local cache

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
 var cacheName = 'petstore-v1';
 var cacheFiles = [
     'index.html',
-    'product.js',
-    '/images/yarn.png',
-    '/images/cat-house.png',
-    '/images/cat-litter.png',
-    '/images/laser-pointer.png',
-    '/images/icon-store-512.png'
+    'products.js',
+    'petstore.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png'
 ];
 
 self.addEventListener('install', (e) => {
@@ -15,6 +16,32 @@ self.addEventListener('install', (e) => {
         caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             // 'r' is the matching file if it exists in the cache
+//             return r 
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache, 
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
